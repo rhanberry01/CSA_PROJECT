38ae6f44d8d24f7eb393cfb53e34196dc942be35
rhanberry01 <mohairan.salunayanm@gamil.com>
2020-06-10 10:24:32 +0800

    remove database added on gitignore

--- front_end/src/Database.js
+++
@@ -1,4 +0,0 @@
-// export const BACKENDIP = (window.apiUrl) ? window.apiUrl : "192.168.0.188";
-// export const BACKENDPORT = (window.apiPort) ? window.apiPort : "8888";
-export const BACKENDIP = "192.168.0.141";
-export const BACKENDPORT = "40011";
